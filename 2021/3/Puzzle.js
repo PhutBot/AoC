4ed4ef5e63b14f937e9782414e235cdcd858a3d8
PhutBot <phutbot@gmail.com>
2021-12-02 02:14:09 -0600

    day 3 prep

---
+++ 2021/3/Puzzle.js
@@ -0,0 +1,19 @@
+const aoc = require('../../AoC');
+
+class Day3 extends aoc.Puzzle {
+    constructor() {
+        super(2021, 3, true);
+    }
+
+    part1(input) {
+        // 0
+        // 0
+    }
+
+    part2(input) {
+        // 0
+        // 0
+    }
+}
+
+new Day3().run();
