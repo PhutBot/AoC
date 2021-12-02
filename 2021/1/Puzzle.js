47834417e744195f39bb4901ef54e8f84bd40cc1
PhutBot <phutbot@gmail.com>
2021-12-02 02:13:30 -0600

    day 1

---
+++ 2021/1/Puzzle.js
@@ -0,0 +1,32 @@
+const aoc = require('../../AoC');
+
+class Day1 extends aoc.Puzzle {
+    constructor() {
+        super(2021, 1, false);
+        this.prep = aoc.MapStringToInt;
+    }
+
+    part1(input) {
+        // 7
+        // 1482
+        let total = 0;
+        let lastDepth = input[0];
+        return input.reduce((prev, curr, i, arr) => {
+            prev += curr < arr[i+1] ? 1 : 0;
+            return prev;
+        }, 0);
+        
+        return total;
+    }
+
+    part2(input) {
+        // 5
+        // 1518
+        return input.reduce((prev, curr, i, arr) => {
+            prev += curr < arr[i+3] ? 1 : 0;
+            return prev;
+        }, 0);
+    }
+}
+
+new Day1().run();
