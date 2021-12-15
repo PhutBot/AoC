27227804faef9266dd04964f1be89c8edc47f2b6
PhutBot <phutbot@gmail.com>
2021-12-14 19:21:33 -0600

    example puzzle folder

---
+++ 2021/00/Puzzle.js
@@ -0,0 +1,17 @@
+const aoc = require('../../AoC');
+
+class Day9999 extends aoc.Puzzle {
+    constructor() {
+        super(2021, 9999, false);
+    }
+
+    part1(input) { // example result: 0
+
+    }
+
+    part2(input) { // example result: 0
+
+    }
+}
+
+new Day9999().run();
